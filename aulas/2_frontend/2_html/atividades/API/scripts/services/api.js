dfd97898c386276a59bf208800947aeb453cc7a7
rosieliz <igorb.kuhl@gmail.com>
2026-07-02 23:31:50 -0300

    feat(musicas): ajusta a chamada da API de músicas

--- aulas/2_frontend/2_html/atividades/API/scripts/services/api.js
+++ aulas/2_frontend/2_html/atividades/API/scripts/services/api.js
@@ -1,1 +1,6 @@
-async function getRecord(name) {}
+async function getRecords(query) {
+    return await fetch(`https://api.discogs.com/database/search?release_title=${query}`)
+        .then(res => res.json())
+        .then(items => items.results)
+        .catch(err => console.error(err));
+}
