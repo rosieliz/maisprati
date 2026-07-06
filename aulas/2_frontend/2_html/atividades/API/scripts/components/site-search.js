a2e181514363039ede5df4e88fac4f55fd3d0b3c
rosieliz <igorb.kuhl@gmail.com>
2026-07-06 17:09:46 -0300

    feat(musicas): cria elementos com resultados da pesquisa

--- aulas/2_frontend/2_html/atividades/API/scripts/components/site-search.js
+++ aulas/2_frontend/2_html/atividades/API/scripts/components/site-search.js
@@ -1,16 +1,59 @@
+
+function clearResults(searchResults) {
+    for (const ch of searchResults.children) {
+        searchResults.removeChild(ch);
+    }
+}
+
+async function renderOptions(query, searchResults) {
+    if (!query.trim()) return;
+
+    const options = await getRecords(query)
+        .then(res => res)
+        .catch(err => console.error("Render error:", err));
+    if (!options) {
+        console.log("Nenhum lançamento encontrado.");
+        return;
+    }
+
+    clearResults(searchResults);
+
+    options.forEach(opt => {
+        const optionItem = document.createElement("div")
+
+        const optionImage = document.createElement("img");
+        optionImage.src = opt.thumbnail
+
+        const optionText = document.createElement("span");
+        optionText.innerText = opt.text;
+
+        optionItem.appendChild(optionImage);
+        optionItem.appendChild(optionText);
+
+        searchResults.appendChild(optionItem);
+    });
+}
+
 class Search extends HTMLElement {
     connectedCallback() {
+        this.render();
+    }
+
+    async render() {
         this.innerHTML =
             `<div class="input-area">
                 <input
                     id="query-search"
                     type="text"
                     placeholder="Pesquise por título..."
-                    onblur="getRecords(this.value)"
                 >
+                <div id="search-results"></div>
             </div>`;
+
+        document.querySelector("#query-search").addEventListener("blur", async (event) => {
+            renderOptions(event.target.value, document.querySelector("#search-results"));
+        });
     }
 }
 
 customElements.define("site-search", Search);
-
--- aulas/2_frontend/2_html/atividades/API/scripts/services/api.js
+++ aulas/2_frontend/2_html/atividades/API/scripts/services/api.js
@@ -1,6 +1,26 @@
 async function getRecords(query) {
-    return await fetch(`https://api.discogs.com/database/search?release_title=${query}`)
+    const results = await fetch(`https://api.discogs.com/database/search?release_title=${query}`)
         .then(res => res.json())
-        .then(items => items.results)
-        .catch(err => console.error(err));
+        .then(res => res.results)
+        .catch(err => console.error("Result error:", err));
+
+    if (!results) return [];
+
+    const releases = results.map(async result => {
+        const release = await fetch(`https://api.discogs.com/releases/${result.id}`)
+            .then(res => res.json())
+            .catch(err => console.error("Resource error:", err));
+
+        if (!release?.title) return;
+
+        const thumbnail = release.thumb ?? "https://placehold.co/40/c89fb5/c89fb5";
+        const artists = release.artists.map(a => a.name.replace(/\s\(.+\)/g, "")).join(", ");
+        const text = `${artists} - ${release.title}`;
+
+        const obj = { text, thumbnail };
+        return obj;
+    });
+
+    const promises = await Promise.all(releases);
+    return promises.filter(p => p);
 }
