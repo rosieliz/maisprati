2150bb7a78fd7d147431f4e4c2d61439c08a6ccc
rosieliz <igorb.kuhl@gmail.com>
2026-07-21 17:31:45 -0300

    feat(musicas): tenta coletar letras pelo Genius

--- aulas/2_frontend/2_html/atividades/API/ongakusei/src/app/api/music/utils.ts
+++ aulas/2_frontend/2_html/atividades/API/ongakusei/src/app/api/music/utils.ts
@@ -1,5 +1,7 @@
 "use cache";
 
+// @ts-ignore
+import { getLyrics } from "genius-lyrics-api";
 import type { ISong, IHitResult } from "@/types/api.types";
 
 async function fetchSongs(query: string, page: string): Promise<ISong[]> {
@@ -29,13 +31,36 @@ async function fetchSongs(query: string, page: string): Promise<ISong[]> {
 async function fetchLyrics(song: ISong): Promise<string> {
   if (song.lyrics) return song.lyrics;
 
+  console.log("Attempting to scrape lyrics from Genius...");
+
+  const geniusPageContent = await fetch(song.url).then((res) => res.text());
+  if (!geniusPageContent.includes("data-dnt")) return scrapeGeniusLyrics(song);
+
+  console.log("Genius scraping blocked. Falling back to LRCLIB...");
+  return fetchLyricsWithLrclib(song);
+}
+
+async function scrapeGeniusLyrics(song: ISong): Promise<string> {
+  const scrapedResponse: string = await getLyrics({
+    apiKey: process.env.GENIUS_ACCESS_KEY,
+    title: song.title,
+    artist: song.artists,
+    optimizeQuery: true,
+  });
+  return scrapedResponse
+    .replace(/\d+.*Read More\s/, "")
+    .replace(/\d+\sContributor(s?).*Lyrics/, "")
+    .replace(/\d+\sContributor(s?)$/, "");
+}
+
+async function fetchLyricsWithLrclib(song: ISong): Promise<string> {
   const url = new URL(
     `https://lrclib.net/api/search?track_name=${song.title}&artist_name=${song.artists}`,
   ).href;
   const data = await fetch(url)
     .then((res) => res.json())
     .catch((err) => console.error("Error fetching lyrics:", err));
-  return data[0]?.plainLyrics || "";
+  return data?.[0]?.plainLyrics || "";
 }
 
 export { fetchSongs, fetchLyrics };
